4673bd9f3bd87b627636343acc57ba00947ccbb7
Daniel Hernandez <danny20031405@gmail.com>
2026-01-17 00:31:51 -0600

    update favicon name

---
+++ src/data/proyects.ts
@@ -0,0 +1,10 @@
+export const proyects = [
+    {
+        name: "Rally Latinoamericano de Innovación 2025",
+        description: "Proyecto ganador del primer lugar en impacto social en el Rally Latinoamericano de Innovación 2025 (sede UTEC). Desarrollo autónomo de una web pública como apoyo a la presentación del proyecto.",
+        tecnologies: ['HTML', 'CSS', 'JS']
+    },
+    {
+        name: "Restaurante Taco Express"
+    }
+]

99bc5fffcc8427efcfc5c4d80b0c2c5c3afa5573
Daniel Hernandez <danny20031405@gmail.com>
2026-01-18 00:00:00 -0600

    Add new cards

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -1,10 +1,21 @@
+import taco_express from '@/assets/taco-express.jpg'
+
+
+
 export const proyects = [
     {
+        img:"",
         name: "Rally Latinoamericano de Innovación 2025",
-        description: "Proyecto ganador del primer lugar en impacto social en el Rally Latinoamericano de Innovación 2025 (sede UTEC). Desarrollo autónomo de una web pública como apoyo a la presentación del proyecto.",
+        description: "Proyecto ganador del primer lugar en impacto social en el Rally Latinoamericano de Innovación 2025 (sede UTEC).",
+        link:"",
         tecnologies: ['HTML', 'CSS', 'JS']
     },
     {
-        name: "Restaurante Taco Express"
-    }
+        img:taco_express.src,
+        name: "Restaurante Taco Express (Cliente Real)",
+        description: "Sistema de pedidos que genera órdenes automáticas de WhatsApp desde un menú digital.",
+        link:"https://taco-express-web-site.pages.dev/",
+        tecnologies:['ASTRO','TAILWIND','TYPESCRIPT', 'SUPABASE','Preact']
+    },
+    
 ]
